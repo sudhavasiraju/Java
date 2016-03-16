57888f66174a90729f63f8d20c9bbe618734c953
Sudhakar Vasiraju <Sudhakar.Vasiraju@bazaarvoice.com>
2016-03-16 13:42:05 -0700

    Test

---
+++ Java/src/com/test/StringExpansion.java
@@ -0,0 +1,23 @@
+import java.lang.System;
+
+public class StringExpansion {
+
+    public static void main(String[] args) {
+        new StringExpansion();
+    }
+
+    public StringExpansion(){
+        sop(expand("a4b3c5"));
+    }
+
+    public String expand(String input) {
+        StringBuffer returnVal = new StringBuffer();
+
+
+        return returnVal.toString();
+    }
+
+    public void sop(Object o) {
+        System.out.println(o);
+    }
+}
