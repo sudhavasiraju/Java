4624fbb5c5a92bb4740f2d1df6186f317737503d
Sudhakar Vasiraju <Sudhakar.Vasiraju@bazaarvoice.com>
2016-03-16 21:03:53 -0700

    String stuff

---
+++ sources/src/com/sample/StringExpansion.java
@@ -0,0 +1,103 @@
+package com.sample;
+
+/**
+ * Created by sudhakar.vasiraju on 3/16/16.
+ */
+
+import java.lang.System;
+
+public class StringExpansion {
+
+    public static void main(String[] args) {
+        new StringExpansion();
+    }
+
+    public StringExpansion(){
+        //sop(charCountInString("3"));
+        //sop(expandStringForNums("a4b13c0d4"));
+    }
+
+    public String charCountInString(String input) {
+        StringBuffer returnVal = new StringBuffer();
+
+        if (input != null && input.length() > 0) {
+            if (input.length() == 1) {
+                returnVal.append(input.charAt(0));
+                returnVal.append("1");
+            } else {
+                int j = 0, k = 1;
+                for (int i = 1; i < input.length(); i++) {
+                    if (input.charAt(j) == input.charAt(i)) {
+                        k++;
+                    } else {
+                        returnVal.append(input.charAt(j));
+                        returnVal.append(k);
+                        k = 1;
+                        j = i;
+                    }
+                    if (i == input.length() - 1) {
+                        returnVal.append(input.charAt(j));
+                        returnVal.append(k);
+                    }
+                }
+            }
+        }
+        return  returnVal.toString();
+    }
+
+    public String expandStringForNums(String input) {
+        StringBuffer returnVal = new StringBuffer();
+
+        if (input != null && input.length() > 0 ){
+            int count = 0;
+            Character temp = null;
+            int charCount = 0;
+            for (Character c: input.toCharArray()) {
+                charCount++;
+                if (Character.isDigit(c)) {
+                    count = count * 10 + Character.getNumericValue(c);
+                    if (Character.getNumericValue(c) == 0 ){
+                        if (temp != null ) {
+                            returnVal.deleteCharAt(returnVal.length() - 1);
+                            temp = null;
+                        }
+                    }
+                    if (input.length() == charCount) {
+                        if (count > 0) {
+                            count --;
+                            while (count > 0) {
+                                count--;
+                                if (null != temp) {
+                                    returnVal.append(temp);
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    if (count > 0) {
+                        count --;
+                        while (count > 0) {
+                            count--;
+                            if (null != temp) {
+                                returnVal.append(temp);
+                            }
+                        }
+                    }
+                    if (count == 0) {
+                        returnVal.append(c);
+                        temp = c;
+                        count = 0;
+                    }
+
+
+                }
+            }
+        }
+
+        return returnVal.toString();
+    }
+
+    public void sop(Object o) {
+        System.out.println(o);
+    }
+}
---
+++ sources/src/com/sample/test/StringExpansionTest.java
@@ -0,0 +1,77 @@
+package com.sample.test;
+
+import com.sample.StringExpansion;
+import junit.framework.TestCase;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by sudhakar.vasiraju on 3/16/16.
+ */
+
+public class StringExpansionTest extends TestCase{
+
+    public void testCharCountInString(){
+        StringExpansion se = new StringExpansion();
+        String result = se.charCountInString("aaaabbccccdddd");
+
+        assertEquals("Expected out put is a4b2c4d4","a4b2c4d4",result);
+    }
+
+    public void testCharCountInString1(){
+        StringExpansion se = new StringExpansion();
+        String result = se.charCountInString("abababab");
+
+        assertEquals("Expected out put is a1b1a1b1a1b1a1b1","a1b1a1b1a1b1a1b1",result);
+    }
+
+    public void testCharCountInString2(){
+        StringExpansion se = new StringExpansion();
+        String result = se.charCountInString("a");
+
+        assertEquals("Expected out put is a1","a1",result);
+    }
+
+    public void testCharCountInString3(){
+        StringExpansion se = new StringExpansion();
+        String result = se.charCountInString("3");
+
+        assertEquals("Expected out put is 31","31",result);
+    }
+
+    public void testExpandStringForNums(){
+        StringExpansion se = new StringExpansion();
+        String result = se.expandStringForNums("a4b13c2d4");
+
+        assertEquals("Expected out put is aaaabbbbbbbbbbbbbccdddd","aaaabbbbbbbbbbbbbccdddd",result);
+    }
+
+    public void testExpandStringForNums1(){
+        StringExpansion se = new StringExpansion();
+        String result = se.expandStringForNums("a4b3c0d2");
+
+        assertEquals("Expected out put is aaaabbbdd","aaaabbbdd",result);
+    }
+
+    public void testExpandStringForNums2(){
+        StringExpansion se = new StringExpansion();
+        String result = se.expandStringForNums("a4b3cd2");
+
+        assertEquals("Expected out put is aaaabbbcdd","aaaabbbcdd",result);
+    }
+
+    public void testExpandStringForNums3(){
+        StringExpansion se = new StringExpansion();
+        String result = se.expandStringForNums("a4b3c$d");
+
+        assertEquals("Expected out put is aaaabbbc$d","aaaabbbc$d",result);
+    }
+
+    public void testExpandStringForNums4(){
+        StringExpansion se = new StringExpansion();
+        String result = se.expandStringForNums("a4b3c2d");
+
+        assertEquals("Expected out put is aaaabbbccd","aaaabbbccd",result);
+    }
+
+}
